b89ae8f57431da3821fafda11644cbafc1e1b6eb
Pranit Patil <pranitvp2408@gmail.com>
2025-06-19 12:34:28 +0530

    Add files via upload

---
+++ 5_variable.py
@@ -0,0 +1,9 @@
+a = 1
+b = 2
+
+print(a+b)
+
+z = "pranit"
+x = "patil"
+
+print(z+x)
---
+++ 6_datatype variable.py
@@ -0,0 +1,7 @@
+a = 12 #identifies a as class <int>
+
+b = 12.34 #identifies b as class <float>
+
+c = "pranit" #identifies a as class <string>
+
+d = True # identifies the boolean 
