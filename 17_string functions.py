9524f4049dd8a0752d7e8fb83f8f405dc1fa319d
Pranit Patil <pranitvp2408@gmail.com>
2025-06-25 12:37:30 +0530

    Add files via upload

---
+++ 17_string functions.py
@@ -0,0 +1,9 @@
+name = "pranit"
+
+print(len(name))
+
+print(name.endswith("nit"))
+
+print(name.startswith("apra"))
+
+print(name.find("a"))
---
+++ 18_f string.py
@@ -0,0 +1,3 @@
+name = input("enter the name:")
+
+print(f"good afternoon {name}")
