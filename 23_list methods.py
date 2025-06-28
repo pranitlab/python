5b00ccd0dd6dc09deaaf41568cd90c2ae706fc99
Pranit Patil <pranitvp2408@gmail.com>
2025-06-28 21:32:56 +0530

    Add files via upload

---
+++ 23_list methods.py
@@ -0,0 +1,32 @@
+name = ["apple","orange",23,False,23.44,"banana"]
+
+print(name)
+
+name.append("pranit")
+print(name)
+
+l1 = [2 ,54,63,3,1,99]
+
+l1.sort()
+print(l1)
+
+l1 = [2 ,54,63,3,1,99]
+
+l1.reverse()
+print(l1)
+
+l1 = [2 ,54,63,3,1,99]
+
+l1.insert(3,33333)
+print(l1)
+
+l1 = [2 ,54,63,3,1,99]
+
+l1.pop(5)
+print(l1)
+
+
+l1 = [2 ,54,63,3,1,99]
+
+l1.remove(2)
+print(l1)
---
+++ 24_tuple.py
@@ -0,0 +1,26 @@
+name = (1,"pranit","apple",23,23.5,"banana")
+print(name)
+print(type(name))
+
+name = (1,"pranit","apple",23,23.5,"banana")
+print(name.count(23))
+
+name = (1,"pranit","apple",23,23.5,"banana")
+print(name.index("apple"))
+
+
+t = (1, 2, 3)
+print(2 in t)  # Output: True
+
+
+t = (1, 2)
+print(t * 3)  
+
+t = (10, 20, 30)
+a, b, c = t
+print(a, b, c)
+
+
+t = (10, 20, 30, 40)
+print(t[1:3]) 
+ 
