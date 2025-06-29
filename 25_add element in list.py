eb5134b17998b2d7f4bbe18edaf1a8e0f601c269
Pranit Patil <pranitvp2408@gmail.com>
2025-06-29 11:07:41 +0530

    Add files via upload

---
+++ 25_add element in list.py
@@ -0,0 +1,16 @@
+fruit =[]
+
+f1 = input("enter the fruit : ")
+fruit.append(f1)
+f2 = input("enter the fruit : ")
+fruit.append(f2)
+f3 = input("enter the fruit : ")
+fruit.append(f3)
+f4 = input("enter the fruit : ")
+fruit.append(f4)
+f5 = input("enter the fruit : ")
+fruit.append(f5)
+f6 = input("enter the fruit : ")
+fruit.append(f6)
+
+print(fruit)
