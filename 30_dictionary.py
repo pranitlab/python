bd59fe90a1a7f955d30129ecff6fff24d4da77b6
Pranit Patil <pranitvp2408@gmail.com>
2025-07-02 15:39:50 +0530

    Add files via upload

---
+++ 30_dictionary.py
@@ -0,0 +1,5 @@
+person = {"name": "John", "age": 30}
+print(person["name"])        # Output: John
+print(person.get("age"))     # Output: 30
+person["age"] = 31           # Modify value
+person["city"] = "Mumbai"    # Add new key-value pair
