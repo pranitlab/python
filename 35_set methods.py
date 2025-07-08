dfe1bf36f8bb70bae19db12ad83477abf8c36b06
Pranit Patil <pranitvp2408@gmail.com>
2025-07-08 19:51:23 +0530

    Add files via upload

---
+++ 35_set methods.py
@@ -0,0 +1,10 @@
+e = set
+s = {32,1,5,54,7,5}
+
+#s.add(566)
+#s.update([32,55])
+#s.discard(1)
+#s.remove(32)
+s.clear()
+
+print(s)
---
+++ 36_set union intersection.py
@@ -0,0 +1,6 @@
+s1 = {1,2,3,4,5}
+s2 = {4,5,6,7,8,9}
+
+print(s1.union(s2))
+
+print(s1.intersection(s2))
