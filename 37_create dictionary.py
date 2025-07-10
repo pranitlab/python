a40088256ab37ac9ea3986df8a955810bd782898
Pranit Patil <pranitvp2408@gmail.com>
2025-07-10 22:39:41 +0530

    Add files via upload

---
+++ 37_create dictionary.py
@@ -0,0 +1,10 @@
+words = {
+    "madat":"help",
+    "khurchi":"chair",
+    "kutta":"dog"
+    
+}
+
+word = input("enter the word want meaninig of ")
+
+print(words[word])
---
+++ 38_enter the numbers.py
@@ -0,0 +1,28 @@
+s = set()
+
+
+num = input("enter the number : ")
+s.add(int(num))
+
+num = input("enter the number : ")
+s.add(int(num))
+
+num = input("enter the number : ")
+s.add(int(num))
+
+num = input("enter the number : ")
+s.add(int(num))
+
+num = input("enter the number : ")
+s.add(int(num))
+
+num = input("enter the number : ")
+s.add(int(num))
+
+num = input("enter the number : ")
+s.add(int(num))
+
+num = input("enter the number : ")
+s.add(int(num))
+
+print(num)
