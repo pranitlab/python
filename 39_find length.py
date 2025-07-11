ab17610644f48dc4b88ee70efe7e14af6aeecf62
Pranit Patil <pranitvp2408@gmail.com>
2025-07-11 22:06:40 +0530

    Add files via upload

---
+++ 39_find length.py
@@ -0,0 +1,8 @@
+s = set()
+s.add(18)
+s.add(18.0)
+s.add("18")
+
+print(s)
+
+print(len(s))
---
+++ 40_create an dic and add input.py
@@ -0,0 +1,19 @@
+d = {}
+
+name = input("enter firends name :")
+lang = input("enter the language :")
+d.update({name:lang})
+
+name = input("enter firends name :")
+lang = input("enter the language :")
+d.update({name:lang})
+
+name = input("enter firends name :")
+lang = input("enter the language :")
+d.update({name:lang})
+
+name = input("enter firends name :")
+lang = input("enter the language :")
+d.update({name:lang})
+
+print(d)
