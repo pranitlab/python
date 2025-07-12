a527b5b56b03b28519d78b2afca2b9056a6fa55b
Pranit Patil <pranitvp2408@gmail.com>
2025-07-12 15:44:55 +0530

    Add files via upload

---
+++ 41_conditionals.py
@@ -0,0 +1,10 @@
+a = int(input("enter the age : "))
+
+if(a>=18):
+    print("above the age of concent")
+    print("good for you")
+    
+    
+else:
+    print("below the age of concent")
+    print("end program")
---
+++ 42_if_elif_else_ladder.py
@@ -0,0 +1,16 @@
+a = int(input("enter the age : "))
+
+#if else elif ladder
+if(a>=18):
+    print("above the age of concent")
+    print("good for you")
+    
+elif(a<0):
+    print("you are entering invalid input")
+    
+elif(a==0):
+    print("entering invalid age")
+    
+else:
+    print("below the age of concent")
+    print("end program")
