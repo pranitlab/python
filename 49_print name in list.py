998794a69e334555301887d13cd4bc6ef8af4438
Pranit Patil <pranitvp2408@gmail.com>
2025-07-16 19:05:34 +0530

    Add files via upload

---
+++ 49_print name in list.py
@@ -0,0 +1,9 @@
+l = ["pranit","vivek","patil","swag"]
+
+name = input("enter the name")
+
+if(name in l):
+    print("it is present")
+    
+else:
+    print("not present")
---
+++ 50_find grades of input.py
@@ -0,0 +1,19 @@
+marks = int(input("enter the marks"))
+
+if(marks<=100 and marks>=90):
+    print("EX")
+    
+elif(marks<=90 and marks>=80):
+    print("A")
+    
+elif(marks<=80 and marks>=70):
+    print("B")
+
+elif(marks<=70 and marks>=60):
+    print("c")
+    
+elif(marks<=60 and marks>=50):
+    print("p")
+
+elif(marks<50 ):
+    print("fail ")
