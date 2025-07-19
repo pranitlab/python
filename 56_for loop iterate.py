815626c721bb3b9c7f78e85f8536161cffedbee7
Pranit Patil <pranitvp2408@gmail.com>
2025-07-19 13:23:53 +0530

    Add files via upload

---
+++ 56_for loop iterate.py
@@ -0,0 +1,14 @@
+#for loop with list
+l = [1,2,4,6,8,0]
+for i in l:
+    print(i)
+
+#for loop with tuple    
+t = (3,6,80,90)
+for i in t:
+    print(i)
+
+#for loop with string
+s = ("pranit")
+for i in s:
+    print(i)
