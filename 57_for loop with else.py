eb5d43b33825b8e3a7d130ce8e7bcb61f2203850
Pranit Patil <pranitvp2408@gmail.com>
2025-07-22 14:32:28 +0530

    Add files via upload

---
+++ 57_for loop with else.py
@@ -0,0 +1,8 @@
+l = [2,4,8,9]
+
+for i in l:
+    print(i)
+    
+else:
+    print("done")  #this is printed when file exaust
+    
---
+++ 58_break_continue.py
@@ -0,0 +1,11 @@
+for i in range (100):
+    if(i ==30):
+        break #break the loop right now
+    
+    print(i)
+    
+for i in range (100):
+    if(i ==30):
+        continue #skip this iteration
+    
+    print(i)   
