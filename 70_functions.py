c0886b8adc4ec9aad50626b4bb9edda3debe38c3
Pranit Patil <pranitvp2408@gmail.com>
2025-07-28 22:21:00 +0530

    Add files via upload

---
+++ 70_functions.py
@@ -0,0 +1,32 @@
+#a = int(input("enter the number:"))
+#b = int(input("enter the number:"))
+#c = int(input("enter the number:"))
+
+#average = (a + b + c)/3
+#print(average)
+
+
+#a = int(input("enter the number:"))
+#b = int(input("enter the number:"))
+#c = int(input("enter the number:"))
+
+#average = (a + b + c)/3
+#print(average)
+
+
+def avg(): #function defination
+    a = int(input("enter the number:"))
+    b = int(input("enter the number:"))
+    c = int(input("enter the number:"))
+
+    average = (a + b + c)/3
+    print(average)
+    
+avg()  #function call
+avg()
+avg()
+
+
+    
+
+
