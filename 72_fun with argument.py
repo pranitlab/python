1836dfcbafa3df7f74f0b262827fc9b170ba5b5a
Pranit Patil <pranitvp2408@gmail.com>
2025-07-29 14:08:49 +0530

    Add files via upload

---
+++ 72_fun with argument.py
@@ -0,0 +1,8 @@
+def goodday(name ,ending):
+    print("good day " +name)
+    print(ending)
+    
+    
+goodday("harry " ,"thank you")
+goodday("pranit " ,"thank you")
+goodday("pranav " ,"thank you")
