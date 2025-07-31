aebfabebd35ccfd35a752a5d8878a2ab33ca73f7
Pranit Patil <pranitvp2408@gmail.com>
2025-07-31 12:16:42 +0530

    Add files via upload

---
+++ 75_greatest of three num.py
@@ -0,0 +1,13 @@
+def greatest(a , b , c):
+    if (a>b and a>c):
+        return a
+    elif (a>b and a>c):
+        return a
+    else:
+        return c
+    
+a = 1
+b = 2
+c = 4
+
+print(greatest(a,b,c))
