8a4cc2dbeb4e220b57fb30003033324bdc574c2a
Pranit Patil <pranitvp2408@gmail.com>
2025-09-17 22:43:11 +0530

    Add files via upload

---
+++ 95_instance attribute.py
@@ -0,0 +1,10 @@
+class Employee:
+    language = "py"    #this is an class attribute
+    salary = 10
+    
+pranit = Employee()
+pranit.language="javascript"   #this is an instance attribute
+print (pranit.language ,pranit.salary)
+
+
+
